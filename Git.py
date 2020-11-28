2eba1e0d0dcc665766d53cf42593c0c243872e04
Alex <ap419837@gmail.com>
2020-11-28 14:10:44 +0500

    4 change

--- Git.py
+++ Git.py
@@ -2,14 +2,43 @@ import sys
 
 from PyQt5 import uic
 from random import randint
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui_file import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(40, 20, 181, 61))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Нарисовать"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Untitled.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
 
     def run(self):
@@ -19,7 +48,7 @@ class MyWidget(QMainWindow):
         qp = QPainter()
         # Начинаем процесс рисования
         qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(int(randint(1, 255)), int(randint(1, 255)), int(randint(1, 255))))
         x = int(randint(25, 200))
         qp.drawEllipse(int(randint(150, 400)), int(randint(150, 400)), x, x)
         qp.end()
